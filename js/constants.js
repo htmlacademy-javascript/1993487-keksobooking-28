9e519fc7a1ae34b31332fb3c986e0714a5fcb137
Оксана <l_oksana@list.ru>
2023-04-29 14:29:02 +0300

    Добавляет константы

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,13 @@
 const LINK_GET_DATA = 'https://28.javascript.pages.academy/keksobooking/data';
+const START_LAT = 35.68950;
+const START_LNG = 139.691710;
+const MARKER_WIDTH = 40;
+const MAIN_MARKER_WIDTH = 52;
 
-export { LINK_GET_DATA };
+export {
+  LINK_GET_DATA,
+  START_LAT,
+  START_LNG,
+  MAIN_MARKER_WIDTH,
+  MARKER_WIDTH
+};
