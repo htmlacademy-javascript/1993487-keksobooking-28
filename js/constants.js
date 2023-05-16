fa81916f0ed78a82dd8f8511f07f3cc7ae70dfca
Оксана <l_oksana@list.ru>
2023-05-16 12:24:44 +0300

    Добавляет константы

--- js/constants.js
+++ js/constants.js
@@ -1,13 +1,20 @@
 const LINK_GET_DATA = 'https://28.javascript.pages.academy/keksobooking/data';
+const LINK_POST_DATA = 'https://28.javascript.pages.academy/keksobooking';
+
 const START_LAT = 35.68950;
 const START_LNG = 139.691710;
 const MARKER_WIDTH = 40;
 const MAIN_MARKER_WIDTH = 52;
+const MAX_POINTS = 10;
+const FILTER_DELAY = 500;
 
 export {
   LINK_GET_DATA,
   START_LAT,
   START_LNG,
   MAIN_MARKER_WIDTH,
-  MARKER_WIDTH
+  MARKER_WIDTH,
+  FILTER_DELAY,
+  MAX_POINTS,
+  LINK_POST_DATA
 };
