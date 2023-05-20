1a597a00fab77e692431f804c0beea2e71fb1800
Оксана <l_oksana@list.ru>
2023-05-20 14:50:29 +0300

    Добавляет функции для появления окна успеха

--- js/popups.js
+++ js/popups.js
@@ -1,5 +1,28 @@
-const showSuccessPopup = () => {
-  console.log('Сообщение об успехе');
+import { isEscapeKey } from './utils.js';
+
+const popupSuccess = document.querySelector('#success').content.querySelector('.success');
+const bodyElement = document.querySelector('body');
+
+const popupSuccessMessage = popupSuccess.cloneNode(true);
+
+const onModalEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    onCloseModalClick();
+  }
 };
+
+const showSuccessPopup = () => {
+  bodyElement.append(popupSuccessMessage);
+  document.addEventListener('keydown', onModalEscKeydown);
+  popupSuccessMessage.addEventListener('click', onCloseModalClick);
+
+};
+
+function onCloseModalClick() {
+  document.removeEventListener('keydown', onModalEscKeydown);
+  popupSuccessMessage.removeEventListener('click', onCloseModalClick);
+  popupSuccessMessage.remove();
+}
 
 export {showSuccessPopup};
