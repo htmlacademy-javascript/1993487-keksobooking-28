086f282974d8c801551e04a0634e52c3d4cf21c1
Оксана <l_oksana@list.ru>
2023-05-20 14:41:20 +0300

    Дорабатывает карточку отрисовки обьявлений

--- js/card.js
+++ js/card.js
@@ -1,11 +1,14 @@
+import { HOUSING_DICTIONARY } from './constants.js';
+
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const mapContainer = document.querySelector('#map-canvas');
 
 const renderPopup = (popupElement, offer) => {
+  popupElement.querySelector('.popup__avatar').src = offer.author.avatar;
   popupElement.querySelector('.popup__title').textContent = offer.offer.title;
   popupElement.querySelector('.popup__text--address').textContent = offer.offer.address;
   popupElement.querySelector('.popup__text--price').textContent = `${offer.offer.price} ₽/ночь`;
-  popupElement.querySelector('.popup__type').textContent = offer.offer.type;
+  popupElement.querySelector('.popup__type').textContent = HOUSING_DICTIONARY[offer.offer.type];
   popupElement.querySelector('.popup__text--capacity').textContent = `${offer.offer.rooms} комнаты для ${offer.offer.guests} гостей`;
   popupElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.offer.checkin}, выезд до ${offer.offer.checkout}`;
   popupElement.querySelector('.popup__description').textContent = offer.offer.description;
