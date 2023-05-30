96fcb44334bc24f8f8f22906ee8a5c654db330e2
Оксана <l_oksana@list.ru>
2023-05-30 17:45:15 +0300

    Добавляет константы

--- js/constants.js
+++ js/constants.js
@@ -4,6 +4,9 @@ const DEFAULT_AVATAR = 'img/muffin-grey.svg';
 
 const START_LAT = 35.68950;
 const START_LNG = 139.691710;
+const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const COPYRIGHT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const ZOOM = 13;
 const MARKER_WIDTH = 40;
 const MAIN_MARKER_WIDTH = 52;
 const MAX_POINTS = 10;
@@ -11,6 +14,7 @@ const FILTER_DELAY = 500;
 const MIN_LENGTH = 30;
 const MAX_LENGTH = 100;
 const MAX_PRICE = 100000;
+const ALERT_MESSAGE = 'Не удалось загрузить данные с сервера';
 
 
 const HOUSING_DICTIONARY = {
@@ -33,6 +37,9 @@ export {
   LINK_GET_DATA,
   START_LAT,
   START_LNG,
+  TILE_LAYER,
+  COPYRIGHT,
+  ZOOM,
   MAIN_MARKER_WIDTH,
   MARKER_WIDTH,
   FILTER_DELAY,
@@ -43,5 +50,6 @@ export {
   MAX_LENGTH,
   MAX_PRICE,
   MIN_PRICE,
-  DEFAULT_AVATAR
+  DEFAULT_AVATAR,
+  ALERT_MESSAGE
 };
