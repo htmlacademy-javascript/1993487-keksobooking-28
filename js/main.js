0d51fc5c091b4ff3ca1b30325086b79b6ecfb0dd
Оксана <l_oksana@list.ru>
2023-05-30 16:11:48 +0300

    Добавляет вызовы функций в модуль

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 import { getData } from './api.js';
-import { setActiveState, setInactiveState } from './states.js';
+import { setActiveState, setInactiveState, setActiveStateFilters, setInactiveStateFilters } from './states.js';
 import { loadMap } from './map.js';
 import { setFilters } from './filter.js';
 import { setForm } from './form.js';
+import { getAlertMessage } from './utils.js';
+
 setInactiveState();
+setInactiveStateFilters();
 
 loadMap()
   .then(() => {
     getData()
       .then((data) => {
-        setActiveState();
-        console.log(data);
-        //renderMarkers(data);
         setFilters(data);
+        setActiveState();
+        setActiveStateFilters();
         setForm();
       }).catch(() => {
-        console.log('данные не загружены');
+        getAlertMessage('Не удалось загрузить данные с сервера');
       });
-  })
-  .catch(() => {
-    console.log('карта не загружена');
   });
-
