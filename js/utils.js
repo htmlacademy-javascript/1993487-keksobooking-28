0043bd156c6acfb54a1482e002250bc7959cf42a
Оксана <l_oksana@list.ru>
2023-05-30 15:25:39 +0300

    Добавляет функцию в модуль

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const debounce = (callback, timeoutDelay) => {
@@ -8,4 +10,26 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export { debounce, isEscapeKey };
+const getAlertMessage = (message) => {
+  const alertMessage = document.createElement('div');
+
+  alertMessage.style.zIndex = '100';
+  alertMessage.style.position = 'absolute';
+  alertMessage.style.left = '0';
+  alertMessage.style.top = '0';
+  alertMessage.style.right = '0';
+  alertMessage.style.padding = '10px 3px';
+  alertMessage.style.fontSize = '15px';
+  alertMessage.style.textAlign = 'center';
+  alertMessage.style.backgroundColor = 'grey';
+
+  alertMessage.textContent = message;
+
+  document.body.append(alertMessage);
+
+  setTimeout(() => {
+    alertMessage.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export { debounce, isEscapeKey, getAlertMessage };
